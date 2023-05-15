1e8a842cc0763b951f021da15d74be54cc25e4fa
hijo-lot <hajarlotfi2@gmail.com>
2023-05-15 14:39:52 -0700

    C argc argv

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar -  the character c to stdout
+ * @c:The character to print
+ *
+ * Return: On success 1.
+ 
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
