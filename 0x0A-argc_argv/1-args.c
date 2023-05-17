06efd7019402d67d1463a117d47916a1c3176026
hijo-lot <hajarlotfi2@gmail.com>
2023-05-17 14:48:54 -0700

    C argc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * @argc: command's line arguments
+ * @argv: contain the command line 
+ */
 int main(int argc, char *argv[])
 {
 (void) argv; /*Ignore argv*/
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,41 +1,16 @@
 #include <stdio.h>
 #include "main.h"
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
+int main(int argc, char *argv[])
+{ int x,y,z,result ;
+{	if (argc!=3)
+	{printf("ERror\n");
+	
+	return 1;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
 }
-
+ x=atoi(argv[1]);
+  y=atoi(argv[2]);
+  result=x*y ;
+  printf("%d\n", result);
+  return 0 ; 
+  } 
